24e100ec51605085478c18531a8ba0ade60c4052
Martin Brandl <mjibrandl@gmail.com>
2022-05-19 15:52:08 +0200

    add dapr building blocks

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -8,8 +8,6 @@ $containerAppEnvironmentName = 'conappenv-dapr-demo-build'
 
 az group create -n $rgName -l $location
 
-# az containerapp env dapr-component list -g rg-community-build-conapp --name conappenv-build-dapr
-#az storage account keys list --resource-group $rgName --account-name $storageAccountName --query '[0].value' --out tsv
 az servicebus namespace create --resource-group $rgName --name $serviceBusName --location $location
 
 az storage account create `
@@ -18,7 +16,6 @@ az storage account create `
   --location $location `
   --sku Standard_LRS `
   --kind StorageV2
-
 
 az monitor log-analytics workspace create -g $rgName -n $logAnalyticsName
 
@@ -33,48 +30,48 @@ az containerapp env create `
   --location $location
 
 az containerapp env dapr-component set `
-    --name $containerAppEnvironmentName `
-    --resource-group $rgName `
-    --dapr-component-name dapr-demo-state-store `
-    --yaml statestore.yml
+  --name $containerAppEnvironmentName `
+  --resource-group $rgName `
+  --dapr-component-name dapr-demo-state-store `
+  --yaml statestore.yml
 
 az containerapp env dapr-component set `
-    --name $containerAppEnvironmentName `
-    --resource-group $rgName `
-    --dapr-component-name dapr-demo-pubsub `
-    --yaml pubsub.yml
+  --name $containerAppEnvironmentName `
+  --resource-group $rgName `
+  --dapr-component-name dapr-demo-pubsub `
+  --yaml pubsub.yml
 
 az containerapp create `
-    --name dapr-demo-notification-service `
-    --resource-group $rgName `
-    --environment $containerAppEnvironmentName `
-    --image ghcr.io/whiteducksoftware/dapr-demo/notification-service:latest `
-    --min-replicas 1 `
-    --max-replicas 1 `
-    --enable-dapr `
-    --dapr-app-port 5005 `
-    --dapr-app-id 'dapr-demo-notification-service'
-
-  az containerapp create `
-    --name dapr-demo-message-service `
-    --resource-group $rgName `
-    --environment $containerAppEnvironmentName `
-    --image ghcr.io/whiteducksoftware/dapr-demo/message-service:latest `
-    --min-replicas 1 `
-    --max-replicas 1 `
-    --enable-dapr `
-    --dapr-app-protocol grpc `
-    --dapr-app-port 5002 `
-    --dapr-app-id 'dapr-demo-message-service'
+  --name dapr-demo-notification-service `
+  --resource-group $rgName `
+  --environment $containerAppEnvironmentName `
+  --image ghcr.io/whiteducksoftware/dapr-demo/notification-service:latest `
+  --min-replicas 1 `
+  --max-replicas 1 `
+  --enable-dapr `
+  --dapr-app-port 5005 `
+  --dapr-app-id 'dapr-demo-notification-service'
 
 az containerapp create `
-    --name dapr-demo-client `
-    --resource-group $rgName `
-    --environment $containerAppEnvironmentName `
-    --image ghcr.io/whiteducksoftware/dapr-demo/client:latest `
-    --enable-dapr `
-    --dapr-app-id 'dapr-demo-client' `
-    --min-replicas 1 `
-    --max-replicas 1
+  --name dapr-demo-message-service `
+  --resource-group $rgName `
+  --environment $containerAppEnvironmentName `
+  --image ghcr.io/whiteducksoftware/dapr-demo/message-service:latest `
+  --min-replicas 1 `
+  --max-replicas 1 `
+  --enable-dapr `
+  --dapr-app-protocol grpc `
+  --dapr-app-port 5002 `
+  --dapr-app-id 'dapr-demo-message-service'
+
+az containerapp create `
+  --name dapr-demo-client `
+  --resource-group $rgName `
+  --environment $containerAppEnvironmentName `
+  --image ghcr.io/whiteducksoftware/dapr-demo/client:latest `
+  --enable-dapr `
+  --dapr-app-id 'dapr-demo-client' `
+  --min-replicas 1 `
+  --max-replicas 1
 
 
